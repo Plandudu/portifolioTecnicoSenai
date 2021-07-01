ddecca316e69f48e46870944c13ee55dd141b317
felipe-apio-Souza <83766023+felipe-apio-Souza@users.noreply.github.com>
2021-07-01 09:32:50 -0300

    Create Exe6.sh

---
+++ FundamentosTI/Exemplos/Exe6.sh
@@ -0,0 +1,17 @@
+#Autor: Felipe Apio
+#descrição: Modifique o exercício anterior para aceitar somente valores maiores que 0 para N.
+#Caso o valor informado (para N) não seja maior que 0, deverá ser lido um novo valor para N. 
+#!/bin/bash
+echo "Entre com um valor"
+read n
+while [[ $n -gt 0 ]];
+do
+	echo "Entre com um valor"
+	read n
+do
+
+while [[ $n -gt 0 ]]
+	echo ""$n""
+
+	n=$(($n - 1))
+done
