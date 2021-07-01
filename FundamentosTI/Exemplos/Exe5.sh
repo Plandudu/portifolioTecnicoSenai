c015888ca1222c7fc0e38c0153a76b2efe4e6577
felipe-apio-Souza <83766023+felipe-apio-Souza@users.noreply.github.com>
2021-07-01 09:23:02 -0300

    Create Exe5.sh

---
+++ FundamentosTI/Exemplos/Exe5.sh
@@ -0,0 +1,8 @@
+#Autor: Felipe Apio
+#descrição: Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N #(inclusive). Considere que o N será sempre maior que ZERO.
+num=1
+num_max=10
+while [[ $num_max -ge $num ]];
+do
+	echo ""$num_max""
+done
