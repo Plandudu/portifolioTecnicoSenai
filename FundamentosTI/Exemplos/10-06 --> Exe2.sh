ec0a29d38de6f526ca6cbd1e7b2765b10762a0c0
felipe-apio-Souza <83766023+felipe-apio-Souza@users.noreply.github.com>
2021-07-02 09:02:45 -0300

    Update 10-06 --> Exe2.sh

--- FundamentosTI/Exemplos/10-06 --> Exe2.sh
+++ FundamentosTI/Exemplos/10-06 --> Exe2.sh
@@ -1,10 +1,9 @@
 # autor: Felipe Apio.
-# descrição: Neste exercício nós devíamos criar um vetor com o nome de 3 pessoas de nossa escolha, e fazer um loop utilizando o "while".
+# descrição: Inicializar um vetor com 3 nomes de pessoa da sua escolha. Fazer um loop (utilizando while) para escrever para cada uma das pessoa “Oi, eu sou ____ !” 
 #!/bin/bash
-nomes=("João" "Bruno" "Maicon")
-auxiliar=0
-while [ $auxiliar -lt ${#nomes[@]} ]
+vetor=0
+while [[ $vetor -lt ${#nomes[*]}  ]];
 do
-echo "Oi, eu sou ${nomes[$auxiliar]}"
-auxiliar=$((auxiliar+1))
+        echo "Oi, eu sou ${nomes[$vetor]}"
+        vetor=$(($vetor + 1))
 done
