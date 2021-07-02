0c21da3ee6f692e0f26dc590c365b41b634969a6
felipe-apio-Souza <83766023+felipe-apio-Souza@users.noreply.github.com>
2021-07-02 09:03:24 -0300

    Update 10-06 --> Exe1.sh

--- FundamentosTI/Exemplos/10-06 --> Exe1.sh
+++ FundamentosTI/Exemplos/10-06 --> Exe1.sh
@@ -1,8 +1,7 @@
 # autor: Felipe Apio.
-# descrição: Neste exercício nós deviamos criar um vetor com 3 nomes de pessoas de nossa escolha, e fazer um loop utilizando o "for".
+# descrição: Inicializar um vetor com 3 nomes de pessoa da sua escolha. Fazer um loop (utilizando for) para escrever para cada uma das pessoa “Oi, eu sou ____ !” 
 #!/bin/bash
-nomes=("Maria" "Marcos" "Jose")
-for nome in ${nomes[@]}
+for i in ${nomes[*]};
 do
-echo "Oi, eu sou $nome"
+        echo "Oi, eu sou $i"
 done
