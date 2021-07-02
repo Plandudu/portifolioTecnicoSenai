37fc479c16bed517bc1aa1f29749bd45e828990a
felipe-apio-Souza <83766023+felipe-apio-Souza@users.noreply.github.com>
2021-07-02 09:19:05 -0300

    Update 10-06 --> Exe3.sh

--- FundamentosTI/Exemplos/10-06 --> Exe3.sh
+++ FundamentosTI/Exemplos/10-06 --> Exe3.sh
@@ -2,20 +2,25 @@
 # descricão: Pedir para o usuário nomes de pessoas utilizando loop. Apenas parar de pedir a próxima pessoa quando o usuário digitar um “código de parada” da sua escolha. 
 #Colocar esses nomes de pessoas em um vetor e ao final dizer a quantidade de pessoas digitadas.
 #!/bin/bash
-nomes=""
+nome=""
 echo "Entrar com nomes de pessoas? 1 - sim / 0 - nao"
 read boolean
+i=0
 if [[ $boolean -eq 1 ]];
 then
         while [[ $boolean -eq 1 ]];
         do
                 echo "Entre com o nome de uma pessoa"
-                read nomes
+                read nome[$i]
+                i=$(($i + 1))
+
                 echo "Entrar com nomes de pessoas? 1 - sim / 0 - nao"
                 read boolean
         done
-        for i in ${nomes[*]};
+
+        for i in ${nome[*]};
         do
                 echo "Oi, eu sou $i"
         done
+
 fi
