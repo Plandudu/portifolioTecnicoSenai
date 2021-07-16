94d2606e9954ad62680675c49759c3053197fcf6
Felipe Apio <felipeapiomkt@gmail.com>
2021-07-16 09:00:52 -0300

    adicionando scripts

---
+++ FundamentosTI/ex1_16_07.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Faça um script que me retorne todos os nomes dos usuários do sistema. (Até os que não são seres humanos)
+cat /etc/passwd
---
+++ FundamentosTI/permissionamento.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Faça um script que receba como parâmetro um nome de um arquivo ou diretório e diga quais permissões o usuário dono tem. 
+read conteudo
+ls -l $conteudo
